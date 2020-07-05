ea56a02373402e8b79857991fcc7a778c46e487f
Angar-git <67486632+Angar-git@users.noreply.github.com>
2020-07-05 17:20:48 -0500

    Add files via upload

---
+++ Module-4-solution/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+
+ ( function(window){
+var byeSpeaker = {}
+  byeSpeaker.speakWord= "Good bye";
+
+  byeSpeaker.speak= function(name){
+   console.log(byeSpeaker.speakWord + " " + name);
+  }
+  window.byeSpeaker=byeSpeaker;
+  })(window);
+
